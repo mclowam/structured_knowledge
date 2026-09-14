c6b30538f91b75dd5f1540c59c2fa66e8824c893
mclowam <max.shabelnikov@gmail.com>
2026-09-14 12:12:27 +0500

    made schemas

---
+++ backend/auth/app/schemas/users.py
@@ -0,0 +1,42 @@
+import uuid
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class UserPayload(BaseModel):
+    user_id: uuid.UUID
+    username: str
+    is_staff: bool
+
+
+class UserCreateSchema(BaseModel):
+    username: str
+    password: str
+
+
+class UserResponseSchema(BaseModel):
+    user_id: uuid.UUID
+    username: str
+    is_active: bool
+    is_staff: bool
+
+
+class UserLoginSchema(BaseModel):
+    username: str
+    password: str
+
+
+class LoginResponseSchema(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str
+
+
+class RefreshTokenSchema(BaseModel):
+    refresh_token: str
+
+
+class UpdateUserSchema(BaseModel):
+    username: str | None = None
+    password: str | None = None
