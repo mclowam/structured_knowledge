05b65c24f74dbb358640d17d3493ce2e4a6663c1
mclowam <max.shabelnikov@gmail.com>
2026-09-14 12:10:48 +0500

    made user model

---
+++ backend/auth/app/models/users.py
@@ -0,0 +1,29 @@
+import uuid
+from datetime import datetime
+
+from sqlalchemy import String, Boolean, DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import UUID as PG_UUID
+from app.db.base import Base
+
+
+
+class UserModel(Base):
+    __tablename__ = "users"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        PG_UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
+    )
+    username: Mapped[str] = mapped_column(
+        String(500), unique=True, index=True, nullable=False
+    )
+    password: Mapped[str] = mapped_column(
+        String(600), nullable=False
+    )
+
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
+    is_staff: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
