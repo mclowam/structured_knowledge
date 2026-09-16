93ec7c0d40d697e0de4070cef52e64a9b7af7298
mclowam <max.shabelnikov@gmail.com>
2026-09-16 23:40:40 +0500

    made knowledge types

---
+++ frontend/src/types/knowledge.ts
@@ -0,0 +1,13 @@
+export interface KnowledgeCreate  {
+    title: string,
+}
+
+export interface KnowledgeResponse {
+    id: string,
+    user_id: string,
+    title: string,
+    description?: string,
+    created_at: string,
+    updated_at?: string,
+}
+
