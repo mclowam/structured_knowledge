1d8b9e2fb1153370610037043c7a5462e5893d87
mclowam <max.shabelnikov@gmail.com>
2026-09-19 10:28:27 +0500

    changed some settings

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,10 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/auth': { target: 'http://localhost:8001', changeOrigin: true },
+      '/api': { target: 'http://localhost:8002', changeOrigin: true },
+    },
+  },
 })
